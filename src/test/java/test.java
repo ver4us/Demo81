023cf2998f03b160faa8d6ff386f2a3d42d1f9a8
ver4us <ver4us@gmail.com>
2019-08-01 10:53:12 -0500

    update

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,13 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("asdasd");
+        System.out.println("asdasd");
+
+        System.out.println("asdasd");
+
+        System.out.println("asdasd");
+
+        System.out.println("asdasd");
+
     }
 }
